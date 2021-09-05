b2fcea79e198a2d088d8e1ab7aad4e9b819177b4
Evan Kwan <evanyuekwan@gmail.com>
2021-09-05 19:34:48 -0400

    most of next-level vue 2 finished. lesson 11 done.

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Uh oh!</h1>
+    <h3>Looks like you're experiencing some network issues. Please click the back button and try again</h3>
+    <router-link :to="{ name: 'event-list' }">Or go to home page</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
